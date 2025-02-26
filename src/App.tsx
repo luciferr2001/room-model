6b981dcacdf328703a77cb4a353e62369e9a3323
luciferr2001 <rathodrutik05@gmail.com>
2025-02-26 15:14:31 +0530

    model added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { EarthCanvas } from "./components/model";
+
+function App() {
+  return (
+    <>
+      <EarthCanvas />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/model.tsx
@@ -0,0 +1,30 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stats, useGLTF } from "@react-three/drei";
+
+export const EarthCanvas = () => {
+  const earth = useGLTF("./room/room.glb");
+  return (
+    <Canvas
+      className="cursor-pointer"
+      frameloop="demand"
+      camera={{ position: [-2, 3, 6], fov: 45, near: 0.1, far: 200 }}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        display: "block",
+      }}      
+      shadows // Enable shadows (if needed)
+      performance={{ min: 0.5, max: 1 }} // Control rendering performance
+    >
+      <ambientLight />
+      <OrbitControls enableZoom={true} enablePan={true} />
+      <primitive object={earth.scene} scale={0.5} castShadow={false} receiveShadow={false} />
+      <Stats />
+    </Canvas>
+  );
+};
+
+useGLTF.preload('./room/room.glb')
